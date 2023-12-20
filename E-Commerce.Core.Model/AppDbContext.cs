d27c07e9d813e98722463045e4b8e4e01fdb1db8
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-20 15:15:10 +0300

    UI.WEB : AdminController

--- E-Commerce.Core.Model/AppDbContext.cs
+++ E-Commerce.Core.Model/AppDbContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,10 @@ namespace E_Commerce.Core.Model
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext():base("Data Source=localhost;Initial Catalog=E-CommerceDb;Integrated Security=True;")
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<UserAddress> UsersAddress { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -18,5 +23,11 @@ namespace E_Commerce.Core.Model
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderProduct>OrderProducts { get; set; }
+        public DbSet<OrderPayment> OrderPayments { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ E-Commerce.UI.WEB/Areas/Admin/Controllers/AdminLoginController.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.UI.WEB.Areas.Admin.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        AppDbContext db=new AppDbContext();
+        // GET: Admin/AdminLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Email, string Password)
+        {
+            var data = db.Users.Where(x => x.Email == Email && x.Password == Password && x.IsActive == true && x.IsAdmin == true).ToList();
+            if(data.Count()>0)
+            {
+                Session["AdminLoginUser"]=data.FirstOrDefault();
+                return Redirect("/admin");
+            }
+            else { return View(); }
+        }
+    }
+}
