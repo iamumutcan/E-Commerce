b9b21bfecd760b140d2c8bc3baaeabe767f2c234
Umut Can YILMAZ <202804009@ogr.cbu.edu.tr>
2023-12-23 02:01:01 +0300

    UI.WEB : Added  Categorylist Product List

--- E-Commerce.UI.WEB/Controllers/HomeController.cs
+++ E-Commerce.UI.WEB/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using E_Commerce.Core.Model;
+using E_Commerce.Core.Model.Entity;
+using E_Commerce.UI.WEB.Controllers.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,19 +9,66 @@ using System.Web.Mvc;
 
 namespace E_Commerce.UI.WEB.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : UserControllerBase
     {
+        AppDbContext db = new AppDbContext();
+
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var data=db.Products.OrderByDescending(x => x.CreatedDate).Take(5).ToList();
+            return View(data);
         }
         
         public PartialViewResult GetMenu()
         {
-            var db = new AppDbContext();
             var menus=db.Categories.Where(x=>x.ParentID==0).ToList();
             return PartialView(menus);
+        }
+
+        [Route("UserLogin")]
+        public ActionResult Login() { return View(); }
+
+        [HttpPost]
+        [Route("UserLogin")]
+        public ActionResult Login(string Email,string Password) {
+            var data = db.Users.Where(x => x.Email == Email && x.Password == Password && x.IsActive == true && x.IsAdmin == false).ToList();
+            if (data.Count() > 0)
+            {
+                Session["LoginUser"] = data.FirstOrDefault();
+                return Redirect("/");
+            }
+            else { ViewBag.Error = "You entered an incorrect username or password"; return View(); }
+        }
+
+        [Route("UserRegister")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("UserRegister")]
+        public ActionResult Register(User entity)
+        {
+            try
+            {
+                entity.UpdateDate = DateTime.Now;
+                entity.CreatedDate = DateTime.Now;
+                entity.CreateUserId = 1;
+                entity.IsActive = true;
+                entity.IsAdmin = false;
+                entity.WalletAddress = "XXXX";
+                db.Users.Add(entity);
+                db.SaveChanges();
+                return Redirect("/");
+            }
+            catch (Exception ex)
+            {
+                return View();
+
+            }
+
         }
     }
 }
---
+++ E-Commerce.UI.WEB/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using E_Commerce.Core.Model;
+using E_Commerce.UI.WEB.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.UI.WEB.Controllers
+{
+    public class ProductController : UserControllerBase
+    {
+        AppDbContext db = new AppDbContext();
+        // GET: Product
+        [Route("Product/{title}/{id}")]
+        public ActionResult Detail(int id)
+        {
+            var product = db.Products.Where(x=>x.ID == id).FirstOrDefault();
+
+            return View(product);
+        }
+    }
+}
